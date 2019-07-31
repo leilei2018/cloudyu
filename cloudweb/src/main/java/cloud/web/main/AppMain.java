ada28afd11794c03df6c8394bbe130b1f78f7807
he702170585@qq.com <he702170585@qq.com>
2019-07-31 16:47:19 +0800

    Default Changelist

--- cloudweb/src/main/java/cloud/web/main/AppMain.java
+++ cloudweb/src/main/java/cloud/web/main/AppMain.java
@@ -1,22 +1,30 @@
 package cloud.web.main;
 
-import cloud.web.main.service.HelloService;
+import com.zk.edu.service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
+@EnableFeignClients(basePackages="com.zk.edu.service")
 public class AppMain {
 
     @Autowired
     HelloService helloService;
 
-    @GetMapping("/hello")
+    @PostConstruct
     public String hello(){
 
-        String hello = helloService.hello();
-
-        return hello;
+        for (int i = 0; i < 5; i++) {
+            String hello = helloService.hello();
+            System.out.println("return value->"+hello);
+        }
+        return "";
     }
 
     public static void main(String[] args) {
