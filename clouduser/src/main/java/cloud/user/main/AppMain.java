1258c31fbbafec491b863106df74fc6d80590585
he702170585@qq.com <he702170585@qq.com>
2019-07-31 16:47:02 +0800

    Default Changelist

--- clouduser/src/main/java/cloud/user/main/AppMain.java
+++ clouduser/src/main/java/cloud/user/main/AppMain.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.concurrent.locks.LockSupport;
 
@@ -13,14 +14,13 @@ import java.util.concurrent.locks.LockSupport;
 @RestController
 public class AppMain {
 
-    @GetMapping("/hello")
+    @GetMapping("/hello1")
     public String hello(){
         return "haah";
     }
 
     public static void main(String[] args) {
-
+        RestTemplate res;
         SpringApplication.run(AppMain.class);
-
     }
 }
