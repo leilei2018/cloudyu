cc0a536505981e2cfa5fc5f72eb4f638111a7f03
he702170585@qq.com <he702170585@qq.com>
2019-07-31 16:47:33 +0800

    Default Changelist

--- serviceapi/src/main/java/com/zk/edu/service/HelloService.java
+++ serviceapi/src/main/java/com/zk/edu/service/HelloService.java
@@ -1,5 +1,11 @@
 package com.zk.edu.service;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+@Service
+@FeignClient("user")
 public interface HelloService {
-    String hello();
+    @GetMapping("/hello")
+    public String hello();
 }
